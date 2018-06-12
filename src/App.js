855f5fee9741391eff67c6fefeff5e94eb6521a8
Marcos Rissotto <marcos@MacBook-Pro-de-Marcos.local>
2018-06-12 00:12:34 -0300

    Connect react-redux, containers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
-import LocationList from './components/LocationList';
-import ForecastExtended from './components/ForecastExtended';
-import { setCity } from './actions';
-import { store } from './store';
+import LocationListContainer from './containers/LocationListContainer';
+import ForecastExtendedContainer from './containers/ForecastExtendedContainer';
 
 import './App.css';
 
@@ -20,23 +17,8 @@ const cities = [
 
 
 class App extends Component {
-
-    constructor () {
-        super();
-
-        this.state = { city: null };
-    }
-
-    handleSelectedLocation = city => {
-        this.setState( {city});
-        console.log(`handleWeatherLocationClick ${city}`);
-        
-
-        this.props.setCity(city);
-    };
     
     render(){
-        const { city } = this.state;
         return (
             <MuiThemeProvider>
                 <Grid>
@@ -47,53 +29,22 @@ class App extends Component {
                     </Row>
                     <Row>
                         <Col xs={12} md={6}>
-                            <LocationList cities={cities}
-                            onSelectedLocation={this.handleSelectedLocation}></LocationList>
+                            <LocationListContainer cities={cities}>
+                            </LocationListContainer>
                         </Col>
                         <Col xs={12} md={6}>
                             <Paper zDepth={4} >
                                 <div className='detail'>
-                                    { city &&
-                                        <ForecastExtended city={city}></ForecastExtended>
-                                    }
+                                    <ForecastExtendedContainer></ForecastExtendedContainer>
                                 </div>
                             </Paper>
                         </Col>
                     </Row>
                 </Grid>
-
-                    {/*
-
-                        <Grid fluid>
-                    <Row>
-                        <Col xs={12} sm={6} md={4}>
-                            <div className='red'>MD</div>
-                        </Col>
-                        <Col xs={12} sm={6} md={4}>
-                            <div className='green'>MD</div>
-                        </Col>
-                        <Col xs={12} sm={6} md={4}>
-                            <div className='blue'>MD</div>
-                        </Col>
-                    </Row>
-                </Grid>
-
-                        <div className='pp'>
-                            <LocationList cities={cities}
-                            onSelectedLocation={this.handleSelectedLocation} 
-                            ></LocationList>
-                        </div>
-                    */}
             </MuiThemeProvider>
         );
     }
 }
 
-const mapDispatchToPropsActions = dispatch => ({
-    setCity: value => dispatch(setCity(value))
-});
  
-const AppConnected = connect(null, mapDispatchToPropsActions)(App);
-
-
-export default AppConnected;
+export default App;
